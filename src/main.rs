31ed4714ad263a72d87ea09ed05263eef6a274b2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-03-08 01:43:46 +0100

    restore syzygy tablebase initialization

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 #![feature(futures_api, async_await, await_macro)]
 
-#[macro_use]
-extern crate serde_derive;
+use serde_derive::Deserialize;
 
+use shakmaty::variants::{Atomic, Chess, Giveaway};
 use shakmaty::fen::{Fen, ParseFenError};
+use shakmaty_syzygy::{Tablebase as SyzygyTablebase};
 
 use tide::{App, IntoResponse};
 use tide::head::QueryParams;
 
 use http::status::StatusCode;
 
+use structopt::StructOpt;
+
 use futures_01;
 use futures::task::Poll;
 use futures::future::{self, lazy, poll_fn};
 use futures::compat::Future01CompatExt;
+
+use std::sync::Arc;
+use std::path::PathBuf;
 
 #[derive(Copy, Clone, Debug)]
 enum Variant {
     Standard,
     Atomic,
     Antichess,
+}
+
+#[derive(Debug)]
+struct Tablebases {
+    standard: SyzygyTablebase<Chess>,
+    atomic: SyzygyTablebase<Atomic>,
+    antichess: SyzygyTablebase<Giveaway>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -57,8 +70,55 @@ async fn hello_world(variant: Variant, QueryParams(query): QueryParams<Query>) -
     Ok(r.expect("infallible").into())
 }
 
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// Directory with tablebase files for standard chess.
+    #[structopt(long = "standard", parse(from_os_str))]
+    standard: Vec<PathBuf>,
+    /// Directory with tablebase files for atomic chess.
+    #[structopt(long = "atomic", parse(from_os_str))]
+    atomic: Vec<PathBuf>,
+    /// Directory with tablebase files for antichess.
+    #[structopt(long = "antichess", parse(from_os_str))]
+    antichess: Vec<PathBuf>,
+    /// Directory with Gaviota tablebase files.
+    #[structopt(long = "gaviota", parse(from_os_str))]
+    gaviota: Vec<PathBuf>,
+
+    /// Listen on this address.
+    #[structopt(long = "address", default_value = "127.0.0.1")]
+    address: String,
+    /// Listen on this port.
+    #[structopt(long = "port", default_value = "9000")]
+    port: u16,
+}
+
 fn main() {
-    let mut app = App::new(());
+    // Parse arguments.
+    let opt = Opt::from_args();
+    if opt.standard.is_empty() && opt.atomic.is_empty() && opt.antichess.is_empty() && opt.gaviota.is_empty() {
+        Opt::clap().print_help().expect("usage");
+        println!();
+        return;
+    }
+
+    // Initialize Syzygy tablebases.
+    let mut standard = SyzygyTablebase::<Chess>::new();
+    let mut atomic = SyzygyTablebase::<Atomic>::new();
+    let mut antichess = SyzygyTablebase::<Giveaway>::new();
+
+    for path in opt.standard {
+        standard.add_directory(path).expect("open standard directory");
+    }
+    for path in opt.atomic {
+        atomic.add_directory(path).expect("open atomic directory");
+    }
+    for path in opt.antichess {
+        antichess.add_directory(path).expect("open antichess directory");
+    }
+
+    // Start server.
+    let mut app = App::new(Arc::new(Tablebases { standard, atomic, antichess }));
     app.at("/").get(|q| hello_world(Variant::Standard, q));
     app.serve();
 }
