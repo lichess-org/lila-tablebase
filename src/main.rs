7259af1d537d87a78f6d82addda1f8fa58184f14
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-03-08 02:15:07 +0100

    restore basic probe

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![feature(futures_api, async_await, await_macro)]
 
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 
+use shakmaty::{Outcome, Move, MoveList, Role, Position, Setup, PositionError};
 use shakmaty::variants::{Atomic, Chess, Giveaway};
-use shakmaty::fen::{Fen, ParseFenError};
-use shakmaty_syzygy::{Tablebase as SyzygyTablebase};
+use shakmaty::uci::Uci;
+use shakmaty::san::{San, SanPlus};
+use shakmaty::fen::{Fen, ParseFenError, FenOpts};
+use shakmaty_syzygy::{Tablebase as SyzygyTablebase, Wdl, Dtz, SyzygyError};
 
-use tide::{App, IntoResponse};
+use tide::{App, AppData, IntoResponse};
+use tide::configuration::Configuration;
+use tide::body::Json;
 use tide::head::QueryParams;
 
 use http::status::StatusCode;
@@ -15,24 +20,354 @@ use structopt::StructOpt;
 
 use futures_01;
 use futures::task::Poll;
-use futures::future::{self, lazy, poll_fn};
 use futures::compat::Future01CompatExt;
 
+use arrayvec::ArrayVec;
+
+use std::cmp::{min, Reverse};
 use std::sync::Arc;
 use std::path::PathBuf;
+use std::os::raw::{c_int, c_uchar, c_uint};
 
-#[derive(Copy, Clone, Debug)]
+use log::{info, warn, error};
+
+#[derive(Debug, Copy, Clone)]
 enum Variant {
     Standard,
     Atomic,
     Antichess,
 }
 
-#[derive(Debug)]
+impl Variant {
+    fn position(self, fen: &Fen) -> Result<VariantPosition, PositionError> {
+        match self {
+            Variant::Standard => fen.position().map(VariantPosition::Standard),
+            Variant::Atomic => fen.position().map(VariantPosition::Atomic),
+            Variant::Antichess => fen.position().map(VariantPosition::Antichess),
+        }
+    }
+}
+
+#[derive(Clone)]
+enum VariantPosition {
+    Standard(Chess),
+    Atomic(Atomic),
+    Antichess(Giveaway),
+}
+
+impl VariantPosition {
+    fn borrow(&self) -> &dyn Position {
+        match *self {
+            VariantPosition::Standard(ref pos) => pos,
+            VariantPosition::Atomic(ref pos) => pos,
+            VariantPosition::Antichess(ref pos) => pos,
+        }
+    }
+
+    fn borrow_mut(&mut self) -> &mut dyn Position {
+        match *self {
+            VariantPosition::Standard(ref mut pos) => pos,
+            VariantPosition::Atomic(ref mut pos) => pos,
+            VariantPosition::Antichess(ref mut pos) => pos,
+        }
+    }
+
+    fn uci(&self, m: &Move) -> Uci {
+        match *self {
+            VariantPosition::Standard(ref pos) => Uci::from_move(pos, m),
+            VariantPosition::Atomic(ref pos) => Uci::from_move(pos, m),
+            VariantPosition::Antichess(ref pos) => Uci::from_move(pos, m),
+        }
+    }
+
+    fn san_plus(self, m: &Move) -> SanPlus {
+        match self {
+            VariantPosition::Standard(pos) => SanPlus::from_move(pos, m),
+            VariantPosition::Atomic(pos) => SanPlus::from_move(pos, m),
+            VariantPosition::Antichess(pos) => SanPlus::from_move(pos, m),
+        }
+    }
+}
+
+/// Serialize a `T` (e.g. `Wdl`, `Dtz`) that can be converted to an integer.
+fn into_i32<T, S>(value: &T, s: S) -> Result<S::Ok, S::Error>
+where
+    T: Into<i32> + Clone,
+    S: serde::Serializer,
+{
+    s.serialize_i32(value.clone().into())
+}
+
+/// Serialize an `Option<T>` where `T` can be converted to an integer.
+///
+/// In the future https://github.com/serde-rs/serde/issues/723 might make this
+/// obsolete.
+fn into_i32_option<T, S>(value: &Option<T>, s: S) -> Result<S::Ok, S::Error>
+where
+    T: Into<i32> + Clone,
+    S: serde::Serializer,
+{
+    match value {
+        None => s.serialize_none(),
+        Some(value) => s.serialize_some(&value.clone().into()),
+    }
+}
+
+#[derive(Serialize)]
+struct TablebaseResponse {
+    #[serde(flatten)]
+    pos: PositionInfo,
+    moves: ArrayVec<[MoveInfo; 256]>,
+}
+
+#[derive(Serialize)]
+struct MoveInfo {
+    uci: String,
+    san: String,
+    zeroing: bool,
+    #[serde(skip)]
+    capture: Option<Role>,
+    #[serde(skip)]
+    promotion: Option<Role>,
+    #[serde(flatten)]
+    pos: PositionInfo,
+}
+
+#[derive(Serialize)]
+struct PositionInfo {
+    checkmate: bool,
+    stalemate: bool,
+    variant_win: bool,
+    variant_loss: bool,
+    insufficient_material: bool,
+    #[serde(serialize_with = "into_i32_option")]
+    wdl: Option<Wdl>,
+    #[serde(serialize_with = "into_i32_option")]
+    dtz: Option<Dtz>,
+    dtm: Option<i32>,
+}
+
+#[derive(Serialize)]
+struct MainlineResponse {
+    mainline: Vec<MainlineStep>,
+    winner: Option<char>,
+    #[serde(serialize_with = "into_i32")]
+    dtz: Dtz,
+}
+
+#[derive(Serialize)]
+struct MainlineStep {
+    uci: String,
+    #[serde(serialize_with = "into_i32")]
+    dtz: Dtz,
+}
+
+unsafe fn probe_dtm(pos: &VariantPosition) -> Option<i32> {
+    let pos = match *pos {
+        VariantPosition::Standard(ref pos) => pos,
+        _ => return None,
+    };
+
+    if pos.board().occupied().count() > 5 || pos.castles().any() {
+        return None;
+    }
+
+    if gaviota_sys::tb_is_initialized() == 0 {
+        return None;
+    }
+
+    let mut ws = ArrayVec::<[c_uint; 6]>::new();
+    let mut bs = ArrayVec::<[c_uint; 6]>::new();
+    let mut wp = ArrayVec::<[c_uchar; 6]>::new();
+    let mut bp = ArrayVec::<[c_uchar; 6]>::new();
+
+    for (sq, piece) in pos.board().pieces() {
+        piece.color.fold(&mut ws, &mut bs).push(c_uint::from(sq));
+        piece.color.fold(&mut wp, &mut bp).push(c_uchar::from(piece.role));
+    }
+
+    ws.push(gaviota_sys::TB_squares::tb_NOSQUARE as c_uint);
+    bs.push(gaviota_sys::TB_squares::tb_NOSQUARE as c_uint);
+    wp.push(gaviota_sys::TB_pieces::tb_NOPIECE as c_uchar);
+    bp.push(gaviota_sys::TB_pieces::tb_NOPIECE as c_uchar);
+
+    let mut info: c_uint = 0;
+    let mut plies: c_uint = 0;
+
+    let result = gaviota_sys::tb_probe_hard(
+        pos.turn().fold(gaviota_sys::TB_sides::tb_WHITE_TO_MOVE, gaviota_sys::TB_sides::tb_BLACK_TO_MOVE) as c_uint,
+        pos.ep_square().map_or(gaviota_sys::TB_squares::tb_NOSQUARE as c_uint, c_uint::from),
+        gaviota_sys::TB_castling::tb_NOCASTLE.0,
+        ws.as_ptr(),
+        bs.as_ptr(),
+        wp.as_ptr(),
+        bp.as_ptr(),
+        &mut info as *mut c_uint,
+        &mut plies as *mut c_uint);
+
+    let plies = plies as i32;
+
+    match gaviota_sys::TB_return_values(info) {
+        gaviota_sys::TB_return_values::tb_DRAW if result != 0 => Some(0),
+        gaviota_sys::TB_return_values::tb_WMATE if result != 0 => Some(pos.turn().fold(plies, -plies)),
+        gaviota_sys::TB_return_values::tb_BMATE if result != 0 => Some(pos.turn().fold(-plies, plies)),
+        gaviota_sys::TB_return_values::tb_FORBID => None,
+        _ => {
+            warn!("gaviota probe failed with result {} and info {}", result, info);
+            None
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 struct Tablebases {
-    standard: SyzygyTablebase<Chess>,
-    atomic: SyzygyTablebase<Atomic>,
-    antichess: SyzygyTablebase<Giveaway>,
+    standard: Arc<SyzygyTablebase<Chess>>,
+    atomic: Arc<SyzygyTablebase<Atomic>>,
+    antichess: Arc<SyzygyTablebase<Giveaway>>,
+}
+
+impl Tablebases {
+    fn probe_dtz(&self, pos: &VariantPosition) -> Result<Dtz, SyzygyError> {
+        match *pos {
+            VariantPosition::Standard(ref pos) => self.standard.probe_dtz(pos),
+            VariantPosition::Atomic(ref pos) => self.atomic.probe_dtz(pos),
+            VariantPosition::Antichess(ref pos) => self.antichess.probe_dtz(pos),
+        }
+    }
+
+    fn best_move(&self, pos: &VariantPosition) -> Result<Option<(Move, Dtz)>, SyzygyError> {
+        match *pos {
+            VariantPosition::Standard(ref pos) => self.standard.best_move(pos),
+            VariantPosition::Atomic(ref pos) => self.atomic.best_move(pos),
+            VariantPosition::Antichess(ref pos) => self.antichess.best_move(pos),
+        }
+    }
+
+    fn real_wdl(&self, pos: &VariantPosition, dtz: Dtz) -> Result<Wdl, SyzygyError> {
+        if let Some(outcome) = pos.borrow().outcome() {
+            return Ok(Wdl::from_outcome(outcome, pos.borrow().turn()));
+        }
+
+        let halfmoves = min(101, pos.borrow().halfmoves()) as i32;
+        let before_zeroing = dtz.add_plies(halfmoves);
+
+        if before_zeroing.0.abs() != 100 || halfmoves == 0 {
+            // Unambiguous.
+            return Ok(Wdl::from_dtz_after_zeroing(before_zeroing));
+        }
+
+        if halfmoves == 1 && dtz.0.abs() == 99 {
+            // This could only be a cursed/blessed result if the real DTZ was
+            // 100 instead of 99. But tables with DTZ 100 will always
+            // store precise DTZ values, hence it could not have been 100.
+            return Ok(Wdl::from_dtz_after_zeroing(before_zeroing));
+        }
+
+        let best = self.best_move(pos)?.expect("has moves");
+        let mut after = pos.clone();
+        after.borrow_mut().play_unchecked(&best.0);
+        Ok(-self.real_wdl(&after, best.1)?)
+    }
+
+    fn position_info(&self, pos: &VariantPosition) -> Result<PositionInfo, SyzygyError> {
+        let (variant_win, variant_loss) = match pos.borrow().variant_outcome() {
+            Some(Outcome::Decisive { winner }) =>
+                (winner == pos.borrow().turn(), winner != pos.borrow().turn()),
+            _ =>
+                (false, false),
+        };
+
+        fn user_error_as_none<T>(res: Result<T, SyzygyError>) -> Result<Option<T>, SyzygyError> {
+            match res {
+                Err(SyzygyError::Castling)
+                | Err(SyzygyError::TooManyPieces)
+                | Err(SyzygyError::MissingTable { .. }) => Ok(None), // user error
+                Err(err) => Err(err), // server error
+                Ok(res) => Ok(Some(res)),
+            }
+        }
+
+        let dtz = user_error_as_none(self.probe_dtz(pos))?;
+
+        let wdl = if let Some(dtz) = dtz {
+            user_error_as_none(self.real_wdl(pos, dtz))?
+        } else {
+            None
+        };
+
+        Ok(PositionInfo {
+            checkmate: pos.borrow().is_checkmate(),
+            stalemate: pos.borrow().is_stalemate(),
+            variant_win,
+            variant_loss,
+            insufficient_material: pos.borrow().is_insufficient_material(),
+            dtz,
+            wdl,
+            dtm: unsafe { probe_dtm(pos) },
+        })
+    }
+
+    fn probe(&self, pos: &VariantPosition) -> Result<TablebaseResponse, SyzygyError> {
+        let mut moves = MoveList::new();
+        pos.borrow().legal_moves(&mut moves);
+
+        let mut move_info = moves.iter().map(|m| {
+            let mut after = pos.clone();
+            after.borrow_mut().play_unchecked(m);
+
+            Ok(MoveInfo {
+                uci: pos.uci(m).to_string(),
+                san: pos.clone().san_plus(m).to_string(),
+                pos: self.position_info(&after)?,
+                capture: m.capture(),
+                promotion: m.promotion(),
+                zeroing: m.is_zeroing(),
+            })
+        }).collect::<Result<ArrayVec<[_; 256]>, SyzygyError>>()?;
+
+        move_info.sort_by_key(|m: &MoveInfo| (
+            (Reverse(m.pos.checkmate), Reverse(m.pos.variant_loss), m.pos.variant_win),
+            (Reverse(m.pos.wdl == Some(Wdl::Loss)), m.pos.wdl),
+            (Reverse(m.pos.stalemate), Reverse(m.pos.insufficient_material)),
+            if m.pos.wdl.unwrap_or(Wdl::Draw) < Wdl::Draw { Reverse(m.pos.dtm) } else { Reverse(None) },
+            if m.pos.wdl.unwrap_or(Wdl::Draw) > Wdl::Draw { m.pos.dtm.map(Reverse) } else { None },
+            m.zeroing ^ (m.pos.wdl.unwrap_or(Wdl::Draw) < Wdl::Draw),
+            m.capture.is_some() ^ (m.pos.wdl.unwrap_or(Wdl::Draw) < Wdl::Draw),
+            m.pos.dtz.map(Reverse),
+            (Reverse(m.capture), Reverse(m.promotion)),
+        ));
+
+        Ok(TablebaseResponse {
+            pos: self.position_info(&pos)?,
+            moves: move_info,
+        })
+    }
+
+    fn mainline(&self, mut pos: VariantPosition) -> Result<MainlineResponse, SyzygyError> {
+        let dtz = self.probe_dtz(&pos)?;
+        let mut mainline = Vec::new();
+
+        if dtz != Dtz(0) {
+            while pos.borrow().halfmoves() < 100 {
+                if let Some((m, dtz)) = self.best_move(&pos)? {
+                    mainline.push(MainlineStep {
+                        uci: pos.uci(&m).to_string(),
+                        dtz,
+                    });
+
+                    pos.borrow_mut().play_unchecked(&m);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        Ok(MainlineResponse {
+            dtz,
+            mainline,
+            winner: pos.borrow().outcome().and_then(|o| o.winner()).map(|winner| winner.char()),
+        })
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -46,28 +381,37 @@ impl Query {
     }
 }
 
-async fn hello_world(variant: Variant, QueryParams(query): QueryParams<Query>) -> Result<String, impl IntoResponse> {
+async fn probe(variant: Variant, AppData(tbs): AppData<Tablebases>, QueryParams(query): QueryParams<Query>) -> Result<Json<TablebaseResponse>, impl IntoResponse> {
     let fen = match query.fen() {
         Ok(fen) => fen,
         Err(_) => return Err("fen invalid".with_status(StatusCode::BAD_REQUEST)),
     };
 
-    let old = futures_01::future::lazy(|| futures_01::future::poll_fn(|| {
-        tokio_threadpool::blocking(|| {
-            std::thread::sleep(std::time::Duration::from_millis(10_000));
-            "hello again!"
-        }) // .map_err(|_| panic!("tokio threadpool active"))
+    let pos = match variant.position(&fen) {
+        Ok(pos) => pos,
+        Err(_) => return Err("fen illegal".with_status(StatusCode::BAD_REQUEST)),
+    };
 
-        /* match poll_01 {
-            futures_01::Async::Ready(r) => Poll::Ready(r),
-            futures_01::Async::NotReady => Poll::Pending,
-            //Err(_) => panic!("poll err"),
-        } */
+    match pos {
+        VariantPosition::Standard(ref pos) =>
+            info!("standard: {}", FenOpts::default().fen(pos)),
+        VariantPosition::Atomic(ref pos) =>
+            info!("atomic: {}", FenOpts::default().fen(pos)),
+        VariantPosition::Antichess(ref pos) =>
+            info!("antichess: {}", FenOpts::default().promoted(true).fen(pos)),
+    }
+
+    let old = futures_01::future::lazy(|| futures_01::future::poll_fn(|| {
+        tokio_threadpool::blocking(|| match tbs.probe(&pos) {
+            Ok(res) => Ok(Json(res)),
+            Err(err) => {
+                error!("probe failed: {} ({:?} position {})", err.to_string(), variant, fen);
+                Err("probe failed".with_status(StatusCode::INTERNAL_SERVER_ERROR))
+            }
+        })
     }));
 
-    let r = await!(Future01CompatExt::compat(old));
-
-    Ok(r.expect("infallible").into())
+    await!(Future01CompatExt::compat(old)).expect("tokio threadpool active")
 }
 
 #[derive(StructOpt, Debug)]
@@ -102,23 +446,37 @@ fn main() {
         return;
     }
 
-    // Initialize Syzygy tablebases.
-    let mut standard = SyzygyTablebase::<Chess>::new();
-    let mut atomic = SyzygyTablebase::<Atomic>::new();
-    let mut antichess = SyzygyTablebase::<Giveaway>::new();
+    let config = Configuration::build()
+        .address(opt.address)
+        .port(opt.port)
+        .finalize();
 
-    for path in opt.standard {
-        standard.add_directory(path).expect("open standard directory");
-    }
-    for path in opt.atomic {
-        atomic.add_directory(path).expect("open atomic directory");
-    }
-    for path in opt.antichess {
-        antichess.add_directory(path).expect("open antichess directory");
-    }
+    // Initialize Syzygy tablebases.
+    let tbs = {
+        let mut standard = SyzygyTablebase::<Chess>::new();
+        let mut atomic = SyzygyTablebase::<Atomic>::new();
+        let mut antichess = SyzygyTablebase::<Giveaway>::new();
+
+        for path in opt.standard {
+            standard.add_directory(path).expect("open standard directory");
+        }
+        for path in opt.atomic {
+            atomic.add_directory(path).expect("open atomic directory");
+        }
+        for path in opt.antichess {
+            antichess.add_directory(path).expect("open antichess directory");
+        }
+
+        Tablebases {
+            standard: Arc::new(standard),
+            atomic: Arc::new(atomic),
+            antichess: Arc::new(antichess),
+        }
+    };
 
     // Start server.
-    let mut app = App::new(Arc::new(Tablebases { standard, atomic, antichess }));
-    app.at("/").get(|q| hello_world(Variant::Standard, q));
+    let mut app = App::new(tbs);
+    app.config(config);
+    app.at("/standard").get(|tbs, q| probe(Variant::Standard, tbs, q));
     app.serve();
 }
