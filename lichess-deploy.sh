c1dab124114d29a64e70dbffd3508717c9bed42f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-09-24 12:54:36 +0200

    work around older glibc on server

--- lichess-deploy.sh
+++ lichess-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-cargo build --release
+cargo build --release --target x86_64-unknown-linux-musl
 ssh "root@$1.lichess.ovh" mv /usr/local/bin/lila-tablebase /usr/local/bin/lila-tablebase.bak || (echo "first deploy on this server? set up service and comment out this line" && false)
-scp ./target/release/lila-tablebase "root@$1.lichess.ovh":/usr/local/bin/lila-tablebase
+scp ./target/x86_64-unknown-linux-musl/release/lila-tablebase "root@$1.lichess.ovh":/usr/local/bin/lila-tablebase
 ssh "root@$1.lichess.ovh" systemctl restart lila-tablebase
