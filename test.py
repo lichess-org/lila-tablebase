b778d02211d06af4d156c465fde46bcc9c8fd3b3
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-09-12 16:52:52 +0200

    test mate in 8 piece position

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def standard(fen):
 
 
 class TablebaseTest(unittest.TestCase):
-    def test_mate(self):
+    def test_six_piece_mate(self):
         r = standard("8/4K2k/5Q1P/6P1/8/8/q7/8 w - - 99 148")
         self.assertEqual(r["category"], "win")
         self.assertEqual(r["moves"][0]["san"], "Qg7#")
@@ -30,6 +30,15 @@ class TablebaseTest(unittest.TestCase):
         self.assertEqual(r["category"], "cursed-win")
         self.assertEqual(r["moves"][0]["san"], "Qg7#")
         self.assertEqual(r["moves"][0]["category"], "blessed-loss")
+
+    def test_eight_piece_mate(self):
+        r = standard("7k/7p/1R6/6P1/P7/5n2/4r3/7K b - - 0 41")
+        self.assertEqual(r["category"], "win")
+
+        self.assertEqual(r["moves"][0]["san"], "Rh2#")
+        self.assertEqual(r["moves"][0]["category"], "loss")
+
+        self.assertEqual(r["moves"][2]["category"], "unknown")
 
 
 if __name__ == "__main__":
