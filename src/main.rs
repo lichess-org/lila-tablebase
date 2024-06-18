f659f3aaecf08ff8ee0fb6aa2f48085e52858bf6
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-06-18 21:34:54 +0200

    factor out gaviota

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 #![forbid(unsafe_op_in_unsafe_fn)]
 
 mod filesystem;
+mod gaviota;
 
 use std::{
     cmp::{Ordering, Reverse},
-    ffi::CString,
     net::SocketAddr,
     ops::Neg,
-    os::raw::{c_int, c_uchar, c_uint},
     path::PathBuf,
-    ptr,
     sync::{
         atomic,
         atomic::{AtomicBool, AtomicU64},
@@ -36,7 +34,7 @@ use shakmaty::{
     san::SanPlus,
     uci::Uci,
     variant::{Antichess, Atomic, Chess, Variant, VariantPosition},
-    CastlingMode, EnPassantMode, Move, Outcome, Position, PositionError, Role,
+    CastlingMode, Move, Outcome, Position, PositionError, Role,
 };
 use shakmaty_syzygy::{
     filesystem::Filesystem, AmbiguousWdl, Dtz, MaybeRounded, SyzygyError,
@@ -234,76 +232,6 @@ struct MainlineStep {
     precise_dtz: Option<Dtz>,
 }
 
-unsafe fn probe_dtm(pos: &VariantPosition) -> Option<i32> {
-    let VariantPosition::Chess(ref pos) = pos else {
-        return None;
-    };
-
-    if pos.board().occupied().count() > 5 || pos.castles().any() {
-        return None;
-    }
-
-    if unsafe { gaviota_sys::tb_is_initialized() } == 0 {
-        return None;
-    }
-
-    let mut ws = ArrayVec::<c_uint, 6>::new();
-    let mut bs = ArrayVec::<c_uint, 6>::new();
-    let mut wp = ArrayVec::<c_uchar, 6>::new();
-    let mut bp = ArrayVec::<c_uchar, 6>::new();
-
-    for (sq, piece) in pos.board().clone() {
-        piece.color.fold_wb(&mut ws, &mut bs).push(c_uint::from(sq));
-        piece
-            .color
-            .fold_wb(&mut wp, &mut bp)
-            .push(c_uchar::from(piece.role));
-    }
-
-    ws.push(gaviota_sys::TB_squares::tb_NOSQUARE as c_uint);
-    bs.push(gaviota_sys::TB_squares::tb_NOSQUARE as c_uint);
-    wp.push(gaviota_sys::TB_pieces::tb_NOPIECE as c_uchar);
-    bp.push(gaviota_sys::TB_pieces::tb_NOPIECE as c_uchar);
-
-    let mut info: c_uint = 0;
-    let mut plies: c_uint = 0;
-
-    let result = unsafe {
-        gaviota_sys::tb_probe_hard(
-            pos.turn().fold_wb(
-                gaviota_sys::TB_sides::tb_WHITE_TO_MOVE,
-                gaviota_sys::TB_sides::tb_BLACK_TO_MOVE,
-            ) as c_uint,
-            pos.ep_square(EnPassantMode::Legal)
-                .map_or(gaviota_sys::TB_squares::tb_NOSQUARE as c_uint, c_uint::from),
-            gaviota_sys::TB_castling::tb_NOCASTLE.0,
-            ws.as_ptr(),
-            bs.as_ptr(),
-            wp.as_ptr(),
-            bp.as_ptr(),
-            ptr::addr_of_mut!(info),
-            ptr::addr_of_mut!(plies),
-        )
-    };
-
-    let plies = plies as i32;
-
-    match gaviota_sys::TB_return_values(info) {
-        gaviota_sys::TB_return_values::tb_DRAW if result != 0 => Some(0),
-        gaviota_sys::TB_return_values::tb_WMATE if result != 0 => {
-            Some(pos.turn().fold_wb(plies, -plies))
-        }
-        gaviota_sys::TB_return_values::tb_BMATE if result != 0 => {
-            Some(pos.turn().fold_wb(-plies, plies))
-        }
-        gaviota_sys::TB_return_values::tb_FORBID => None,
-        _ => {
-            warn!("gaviota probe failed with result {result} and info {info}");
-            None
-        }
-    }
-}
-
 #[derive(Debug)]
 struct Tablebases {
     standard: SyzygyTablebase<Chess>,
@@ -358,7 +286,7 @@ impl Tablebases {
             maybe_rounded_dtz: dtz.map(MaybeRounded::ignore_rounding),
             precise_dtz: dtz.and_then(MaybeRounded::precise),
             dtz,
-            dtm: unsafe { probe_dtm(pos) },
+            dtm: unsafe { gaviota::probe_dtm(pos) },
             halfmoves: pos.halfmoves(),
         })
     }
@@ -655,21 +583,7 @@ async fn serve() {
     // Initialize Gaviota tablebase.
     if !opt.gaviota.is_empty() {
         unsafe {
-            assert!(gaviota_sys::tbcache_init(1014 * 1024, 50) != 0);
-            let mut paths = gaviota_sys::tbpaths_init();
-            assert!(!paths.is_null());
-            for path in opt.gaviota {
-                let path = CString::new(path.as_os_str().to_str().unwrap()).unwrap();
-                paths = gaviota_sys::tbpaths_add(paths, path.as_ptr());
-                assert!(!paths.is_null());
-                drop(path);
-            }
-            assert!(!gaviota_sys::tb_init(
-                1,
-                gaviota_sys::TB_compression_scheme::tb_CP4 as c_int,
-                paths
-            )
-            .is_null());
+            gaviota::init(&opt.gaviota);
         }
     }
 
